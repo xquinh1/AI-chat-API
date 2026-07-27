301df7ebe100c032d612ea820fcc40bf1127c7ce
xnqnh0320 <nguyenxuanquynh.2032001@gmail.com>
2026-07-28 00:30:52 +0700

    add storage for chatbot

--- src/services/service.js
+++ src/services/service.js
@@ -1,18 +1,44 @@
 import { GoogleGenAI } from "@google/genai";
+import { history } from "../store/history.js";
 
 class Service {
     constructor() {
+        this.history = history
         this.ai = new GoogleGenAI({
             apiKey: process.env.GEMINI_API_KEY
         })
     }
 
     async askGemini(prompt) {
-        const response = await this.ai.models.generateContent({
-            model: "gemini-3.5-flash",
-            contents: prompt,
-        });
-        return response.text;
+        this.history.push({
+            role: "user",
+            parts: [
+                {
+                    text: prompt
+                }
+            ]
+        })
+
+        try {
+            const response = await this.ai.models.generateContent({
+                model: "gemini-3.5-flash",
+                contents: this.history,
+            });
+    
+            this.history.push({
+                role: "model",
+                parts: [
+                    {
+                        text: response.text
+                    }
+                ]
+            });
+            return response.text;
+        } catch (err) {
+            this.history.pop()
+            throw err
+        }
+
     }
 }
 
